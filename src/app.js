5d63e429fb020f92ad8db199f50ae4bab87437e3
Pedro Alexandre Pacheco <pedro.pacheco.a@gmail.com>
2020-12-14 18:30:29 -0300

    API remedios

--- src/app.js
+++ src/app.js
@@ -1,36 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const classmongodb = require('./mod/classmongo')
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 
-const DB = {
-    antinflamatorios: [
-        {
-            quando: '10:24-12/12/2020',
-            farmacia: 'DROGARIAPACHECO',
-            nome: 'Deocil Sl Diffucap 10 Comprimidos',
-            precovelho: '',
-            preconovo: '',
-            precoregular : '27.19',
-            urlproduto : 'https://drogariaspacheco.vteximg.com.br/arquivos/ids/604291-180-180/VSPM.jpg?v=637064450715100000'
-        },
-        {
-            quando: '10:24-12/12/2020',
-            farmacia: 'DROGARIAPACHECO',
-            nome: 'Paracetamol 20 Comprimidos',
-            precovelho: '',
-            preconovo: '',
-            precoregular : '120.90',
-            urlproduto : 'https://drogariaspacheco.vteximg.com.br/arquivos/ids/604291-180-180/VSPM.jpg?v=637064450715100000'
-        }
-    ]
-};
 
 app.get("/antinflamatorios", async(req,res)=>{
     res.status = 200;
-    res.json(DB)
+    let registros = await classmongodb.todosregistros();
+    res.json(registros);
 });
 
 app.set('porta', process.env.PORT || 3000);
